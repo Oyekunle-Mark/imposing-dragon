aba5aacea4aafc183ba52af7758fc8b4d5729734
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-23 18:25:53 +0100

    Used the flask blueprint

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_restful import Api
 from resources.Student import StudentResource
 
 app = Flask(__name__)
-api = Api(app)
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
 
 api.add_resource(StudentResource, '/', '/<int:id>')
+app.register_blueprint(api_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
