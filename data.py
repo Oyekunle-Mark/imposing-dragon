65992416fd03f481d6c4558ff7ef887b932e5027
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-23 18:05:21 +0100

    The data structure

---
+++ data.py
@@ -0,0 +1,22 @@
+students = [
+    {
+        "name": "John Doe",
+        "age": 15,
+        "sex": "male"
+    },
+    {
+        "name": "Jane Doe",
+        "age": 12,
+        "sex": "female"
+    },
+    {
+        "name": "Lockle Rory",
+        "age": 18,
+        "sex": "male"
+    },
+    {
+        "name": "Seyi Pedro",
+        "age": 10,
+        "sex": "female"
+    }
+]
